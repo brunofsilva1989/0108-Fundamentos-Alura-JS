d6c28465162ff089b7d570f3fdff978f42ac87bc
Bruno F Silva <78174347+brunofsilva1989@users.noreply.github.com>
2023-08-14 23:15:00 -0300

    Avançando no estudo JS 14/08/23

---
+++ JavaScriptXNodeJs.js
@@ -0,0 +1,21 @@
+//Caracteristicas Java Script
+
+//Java Script é uma linguagem de tipagem dinamicamente tipada
+//Sendo q não é necessário declarar o tipo da variável.
+
+//Multiparadigma
+
+//Por que se chama JS?
+
+//O que é ES6 / ES2015?
+//Versão do Java Script
+
+//Linguagem interpretada
+
+//O que é o NodeJS
+//Node é um interpretador do Java Script, que é usado no lado do navegador e q pode ser instalado no 
+//back end instalando ele no seu desktop
+
+
+
+
---
+++ console.js
@@ -0,0 +1,12 @@
+// log => registro
+
+const minhavar = true;
+
+console.log(245)
+console.log("eu sou texto")
+console.log(minhavar)
+
+//tratamento de erro!
+console.log('Erro ao enviar email!');
+
+console.error(new Error("Teste erro"));
