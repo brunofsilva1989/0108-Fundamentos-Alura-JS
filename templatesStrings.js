8c38e7081ef70bd1c97940a2d6453f943acc8bf7
Bruno F Silva <78174347+brunofsilva1989@users.noreply.github.com>
2023-08-15 23:21:23 -0300

    Operadores, Ternário e Template Strings.

---
+++ templatesStrings.js
@@ -0,0 +1,16 @@
+//Oque são templates strings
+
+
+const nome = "Bruno";
+const apresentacao = "Sou o " + nome;
+const cidade = "Santo Andre"
+const idade = 34;
+
+
+
+
+/*Exemplo de template strings*/
+const novaApresentacao = `Eu sou o ${nome} e sou morador de ${cidade}, tenho ${idade} de idade.`
+var d = 
+
+console.log(d)
